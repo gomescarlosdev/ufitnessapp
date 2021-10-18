c82a4e5db2fe5dae542498649ae061af92794111
Carlos Henrique <carlos.henrique.pro@outlook.com>
2021-10-18 12:09:04 -0300

    New Feature Fragment

---
+++ app/src/main/java/br/com/gcdev/ufitness/activity/FragmentsOnBoarding.java
@@ -0,0 +1,14 @@
+package br.com.gcdev.ufitness.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentsOnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments_on_boarding);
+    }
+}
